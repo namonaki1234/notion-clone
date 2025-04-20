605ff1e363375a8f237414508c7192d7a2640236
namonaki1234 <123581321fns1119@gmail.com>
2025-04-21 02:50:01 +0900

    initial commit

---
+++ src/components/Editor.tsx
@@ -0,0 +1,28 @@
+import { ja } from '@blocknote/core/locales';
+import { useCreateBlockNote } from '@blocknote/react';
+import '@blocknote/mantine/style.css';
+import { BlockNoteView } from '@blocknote/mantine';
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string | null; 
+}
+
+function Editor({ onChange, initialContent }: EditorProps) {
+  const editor = useCreateBlockNote({
+    dictionary: ja,
+    initialContent:
+      initialContent != null ? JSON.parse(initialContent) : undefined,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => onChange(JSON.stringify(editor.document))}
+      />
+    </div>
+  );
+}
+
+export default Editor;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [ tailwindcss(),react()],
+  // css: {
+  //   postcss: {
+  //     plugins: [tailwindcss()],
+  //   },
+  
+  // },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
